627aaa7b624f3a94d8f49214d0e92f1c4706456d
unknown <royyanmz87@gmail.com>
2021-04-23 23:15:35 +0700

    Minggu 10

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default{
+    apiKey: "AIzaSyCF_IVcvQR2V0J68-lCQfB2bLPxl_dAhpc",
+    authDomain: "pbf10royyan.firebaseapp.com",
+    projectId: "pbf10royyan",
+    storageBucket: "pbf10royyan.appspot.com",
+    messagingSenderId: "167437132745",
+    appId: "1:167437132745:web:17d3fbcd4f8334684c7e18",
+    measurementId: "G-RE712GL95W"
+  };
